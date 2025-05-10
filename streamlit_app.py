9c598b969114da94ca0f8d8e57fa3a99457ffed6
esraa1109 <ku1192004@gmail.com>
2025-05-10 17:03:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
-import pytesseract
+import easyocr
+
 from PIL import Image
 from pyzbar.pyzbar import decode
 from datetime import datetime
